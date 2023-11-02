0d55b0749c7a6d1d455fcf0f556a2ac20a5b89fe
jeongo0 <jeongo2023911@gamil.com>
2023-11-03 08:22:07 +0900

    Servlet 연습 및 Parameter 연습

---
+++ src/main/java/com/koreaIT/java/am/gugudanServlet.java
@@ -0,0 +1,46 @@
+package com.koreaIT.java.am;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/home/gugudan")
+public class gugudanServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	//get 방식
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String inputDan = request.getParameter("dan");
+		String inputLimit = request.getParameter("limit");
+		String inputColor = request.getParameter("color");
+
+		if (inputDan == null) {
+			inputDan = "1";
+		}
+		
+		if (inputLimit == null) {
+			inputLimit = "1";
+		}
+		
+		if (inputColor == null) {
+			inputColor = "transparent";
+		}
+		
+		int dan = Integer.parseInt(inputDan);
+		int limit = Integer.parseInt(inputLimit);
+		
+		response.getWriter().append(String.format("<div style='color:%s;'>== %d ==</div>", dan));
+		for (int i = 1; i <= limit; i++) {
+			response.getWriter().append(String.format("<div style='color:%s;'>%d * %d = %d</div>", dan, i, dan * i));
+		}
+	}
+}
